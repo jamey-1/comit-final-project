c19448da13ed9fa13768370e974d08cb17af1b04
YEMING LIU <liuyeming@zhouyang.net>
2022-08-12 14:23:37 -0600

    init

---
+++ src/main/java/org/comit/course/ComitFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitFinalProjectApplication.class, args);
+	}
+
+}
